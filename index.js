1a5c5c65928d9c318cb2a87ae02eef5d4af9fc9e
gustavokoz <86996937+gustavokoz@users.noreply.github.com>
2021-12-29 17:04:58 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,44 +1,15 @@
 const token = require('./config.json').token// puxa o token do config.json
+const TOKEN = require('./config.json').token// puxa o token do config.json
 const prefix = require('./config.json').prefix// puxa o prefixo do config.json
+const discord = require('discord.js')
+const fs = require('fs')
 const { Client,Intents,MessageActionRow,MessageAttachment,MessageButton,MessageEmbed,Presence } = require('discord.js')//pega depedencias do discord
 const client = new Client({intents: 32767})// define o client
-client.on('messageCreate', msg =>{
-    let message = msg;
-    let command = msg.content
-    if(command === prefix + "teste1") {
-        msg.reply('teste feito com sucesso')
 
-    }
-    if(command === prefix + "teste2") {
-       let embed = new MessageEmbed()
-       .setAuthor(msg.author.username)
-       .setColor('GREY')
-       .setDescription('DRESCRIÇÃO')
-       .setTitle('titulo')
-       .setFooter('rodapé', msg.author.displayAvatarURL)
-msg.reply({content: 'texto',embeds: [embed]})
-    }
-    if(command === prefix + "teste3") {
-    let row = new MessageActionRow().addComponents(
-        new MessageButton()
-        .setCustomId('1')
-        .setStyle('DANGER')
-        .setLabel('texto')
-    )
-    msg.reply({components: [row], content: 'texto'})
-    }
-})
-client.on('interactionCreate', i =>{
-    if(i.isButton()) {
-        const filter = i => i.customId === "1";
-
-        const collector = i.channel.createMessageComponentCollector({
-          filter
-        });
-        collector.on('collect', i =>{
-            i.reply('oi')
-        })          
-    } 
+   require('./comandos.js')(client)
+   require('./eventos.js')(client)
+client.on('ready' , () =>{
+console.log('\033[32m', client.user.username + ' está online')
 })
 client.on('error', error =>{
     console.log(error)
@@ -46,4 +17,73 @@ client.on('error', error =>{
 process.on("unhandledRejection", error => {
     console.log(error)
     });
+    
+client.once('ready', () => {
+
+
+ 
+  const CLIENT_ID = client.user.id;
+  const rest = new REST({
+      version: '9'
+  }).setToken(TOKEN);
+  (async () => {
+      try {
+          if (!TEST_GUILD_ID) {
+              await rest.put(
+                  Routes.applicationCommands(CLIENT_ID), {
+                      body: commands
+                  },
+              );
+
+          } else {
+              await rest.put(
+                  Routes.applicationGuildCommands(CLIENT_ID, TEST_GUILD_ID), {
+                      body: commands
+                  },
+              );
+
+          }
+      } catch (error) {
+          if (error) console.error(error);
+      }
+  })();
+});
+client.commands = new discord.Collection();
+const commands = [];
+const TEST_GUILD_ID = "";
+const {
+    REST
+} = require('@discordjs/rest');
+const {
+    Routes
+} = require('discord-api-types/v9');
+fs.readdirSync('./slash').forEach(dirs => {
+    const scomandos = fs.readdirSync(`./slash/${dirs}`).filter(files => files.endsWith('.js'));
+for (const file of scomandos) {
+    
+    const command = require(`./slash/${dirs}/${file}`);
+
+    commands.push(command.data.toJSON());
+    client.commands.set(command.data.name, command);
+  
+    
+}})
+
+client.on('interactionCreate', async i => {
+  let interaction = i;
+  i.author = i.user;
+  message = i;
+  i.rep = interaction.followUp
+  if (!interaction.isCommand()) return;
+  await interaction.deferReply();
+  const command = client.commands.get(interaction.commandName);
+  if (!command) return;
+  try {
+      await command.execute(i, client);
+  } catch (error) {
+      if (error) console.error(error);
+      await message.channel.send({ content: 'error', ephemeral: true });
+  }
+})
+
 client.login(token)
